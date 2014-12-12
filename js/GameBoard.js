4841b3311cc74a636f579fef563bb92979aa5180
Nadiyah Ford <nadiyah.ford@gmail.com>
2014-12-11 21:20:43 -0800

    functioning single pc ttt game

--- js/GameBoard.js
+++ js/GameBoard.js
@@ -4,34 +4,110 @@ angular
 
 function GameBoardFunc() {
 
-	var SPACE_STATES = ["unselected", "player1", "player2"];
+	//var SPACE_STATES = ["unselected", "player1", "player2"];
 
 	var GameBoard = function( numSpaces ) {
 		this.numSpaces = numSpaces;
-		this.spaces = new Array( numSpaces );
+		//this.spaces = new Array( numSpaces );
+		this.spaces = [0,0,0,0,0,0,0,0,0];
+
 		this.turnCounter = 0; // Initialized to 0
+		this.win = false;
+		this.playerTurn = true;
+		this.player1 = 0;
+		this.player2 = 0;
 
-		this.togglePace = togglePace;
-		this.getSpaceState = getSpaceState;
-	
+		//this.toggleSpace = toggleSpace;
+		//this.getSpaceState = getSpaceState;
+		this.submitTurn = submitTurn;
+		this.winCheck = winCheck;
+		this.boardReset = boardReset;
+
 		// The toggleSpace function will need to change in order to reflect player selections
+		function submitTurn(i) {
+			console.log(i);
+			console.log("Inside submit turn");
+			console.log("Player turn is set to " + this.playerTurn);
 
-		function togglePace( num ) {
-			this.spaces[num] = (this.spaces[num] + 1) % 2; 
-			this.turnCounter++; // Increments the turn counter
+			if (this.win === false) {
+				if (this.spaces[i] === 0) {
+					if (this.playerTurn === true) {
+						this.spaces[i] = 1;
+						this.spaces[i].innerHTML = "#A1DAEB"; // display an image in the box
+					} else {
+						this.spaces[i] = 7;
+						this.spaces[i].innerHTML = "#A4CF64"; // display an image in the box
+					}
+					console.log(this.spaces);
+					this.playerTurn = !this.playerTurn;
+					// turnCounter++; // Fix the turnCounter
+					this.winCheck();
+				}
+			}
+		} // Closes the submitTurn Block
+
+		function winCheck() {
+			var winner, playAgain;
+
+			// Check player 1 for wins
+			if      (this.spaces[0] + this.spaces[1] + this.spaces[2] === 3 ) { winner = 1;}
+			else if (this.spaces[3] + this.spaces[4] + this.spaces[5] === 3 ) { winner = 1;}
+			else if (this.spaces[6] + this.spaces[7] + this.spaces[8] === 3 ) { winner = 1;}
+			else if (this.spaces[0] + this.spaces[3] + this.spaces[6] === 3 ) { winner = 1;}
+			else if (this.spaces[1] + this.spaces[4] + this.spaces[7] === 3 ) { winner = 1;}
+			else if (this.spaces[2] + this.spaces[5] + this.spaces[8] === 3 ) { winner = 1;}
+			else if (this.spaces[0] + this.spaces[4] + this.spaces[8] === 3 ) { winner = 1;}
+			else if (this.spaces[2] + this.spaces[4] + this.spaces[6] === 3 ) { winner = 1;}
+
+			// Check player 2 for wins
+			if      (this.spaces[0] + this.spaces[1] + this.spaces[2] === 21 ) { winner = 2;}
+			else if (this.spaces[3] + this.spaces[4] + this.spaces[5] === 21 ) { winner = 2;}
+			else if (this.spaces[6] + this.spaces[7] + this.spaces[8] === 21 ) { winner = 2;}
+			else if (this.spaces[0] + this.spaces[3] + this.spaces[6] === 21 ) { winner = 2;}
+			else if (this.spaces[1] + this.spaces[4] + this.spaces[7] === 21 ) { winner = 2;}
+			else if (this.spaces[2] + this.spaces[5] + this.spaces[8] === 21 ) { winner = 2;}
+			else if (this.spaces[0] + this.spaces[4] + this.spaces[8] === 21 ) { winner = 2;}
+			else if (this.spaces[2] + this.spaces[4] + this.spaces[6] === 21 ) { winner = 2;}
+
+			if (winner === 1) {
+				this.player1++;
+				console.log("count of player 1 wins: " + this.player1);
+				this.win = true;
+				// Do you want to play again??
+				playAgain = confirm("Player 1 wins!!! \n\nWant to play again?");
+					if (playAgain === true) { this.boardReset(); } else {}
+			} else if (winner === 2) {
+				this.player2++;
+				this.win = true;
+				// Do you want to play again??
+				playAgain = confirm("Player 2 wins!!! \n\nWant to play again?");
+					if (playAgain === true) { this.boardReset(); } else {}
+			} else {
+				this.win = false;
+			}
+		} // Closes the winCheck block
+
+		function boardReset() {
+			console.log("Inside Board Reset");
+			this.spaces = [0,0,0,0,0,0,0,0,0];
+			this.turnCounter = 0;
+			this.win = false;
+
+			// Sets a random first player
+			var randomNumber = (Math.ceil(Math.random() * 2));
+			if (randomNumber === 2) { this.playerTurn = true; }
+				else { this.playerTurn = false; }
+
+			return this.playerTurn;
+
+			this.submitTurn();
 		}
 
-		function getSpaceState( num ) {
-			return SPACE_STATES[this.spaces[num]];
-		} 
- 	
-		for (var i = 0; i < this.spaces.length; i++) {
+		for (var i = 0; i < this.spaces.length; i++) { // prevents new boxes from being added
 			this.spaces[i] = 0;
 		}
  	}
 
- 	
-
  	return GameBoard;
 
-} // closes the playerMove function	
+} // closes the Game Board function
--- js/GamePlay.js
+++ js/GamePlay.js
@@ -4,11 +4,12 @@ angular
 
 // Game Play keeps track of what happens each time a player checks a box
 
-GamePlayFunc.$inject = ["GameBoard", "GameManager"];
+GamePlayFunc.$inject = ["GameBoard"];
 
-function GamePlayFunc($index) {
+function GamePlayFunc() {
 
 	var self = this;
+	self.submitTurn = submitTurn;
 
 	// self.playerTurn = pickFirstPlayer();
 	
@@ -21,31 +22,38 @@ function GamePlayFunc($index) {
 				playerTurn = false;
 			}
 			return playerTurn;
+			submitTurn();
 		}
 
 		// Controls what happens when a player pics a space
 		function submitTurn($index) {
-			var spaces = document.getElementsByClassName("board-space"); // Turns the board spaces into array
 			console.log("submitTurn");
+			var spaces = document.getElementsByClassName("board-space"); // Turns the board spaces into array
+
+			// Checks to see that there is no winner
 			if (win === false) {
 				console.log("inside win");
+				// Checks to see that the box selected is empty
 				if (playerMoves[$index] === "") { // if the space is blank, assign a value to the move array & place an image
 					console.log("inside player move");
+					// Assigns move to Player 1  or 2 and displays the correct player image
 					if (playerTurn === true) { 
 						console.log("inside player turn 1");
 						playerMoves[$index] = 1; // If Player 1's turn, set the value to 1
-						tttBoard[$index].innerHTML = '<img src=../images/space_marker_dexter>';
+						tttBoard[$index].innerhtml.bgcolor = "#A1DAEB";
+						// tttBoard[$index].innerHTML = '<img src=../images/space_marker_dexter>';
 					} 
 					else { 
 						playerMoves[$index] = 2;  // If Player 2's turn, set the value to 7
-						tttBoard[$index].innerHTML = '<img src=../images/space_marker_robodexo>';
+						tttBoard[$index].innerhtml.bgcolor = "#FFFFFF";
+						// tttBoard[$index].innerHTML = '<img src=../images/space_marker_robodexo>';
 					}
 				} 
 				console.log("Player move function called!");
 				turnCounter++; // Increment the turn counter 	
 				winCheck(); // Check to see if anyone has won the game
 				console.log(turnCounter); // Verify that the turn counter is counting
-			}	
+			}
 				
 		} // closes the playerMove function	
 
@@ -73,25 +81,41 @@ function GamePlayFunc($index) {
 				else if (playerMoves[2], playerMoves[4], playerMoves[6] === 2 ) { winner = 2;}
 
 			if (winner === 1) {
-				player1++; 
+				player1++;
 				win = true;
+				boardReset();
 			} else if (winner === 2) {
 				player2++;
 				win = true;
+				boardReset();
 			} else {
 				win = false;
+				submitTurn(); 
 			}
 
 			if (win === true) {
 				if(confirm("Want to play again?")) {
-					GameManagerFunc();
+					boardReset();
 				} else {
 					alert("Goodbye!"); 
 				}
 			}
 
-			return win; 
-
 		} // closes the winCheck function
+
+		function boardReset(){
+			// What to do when a person has won the game
+			if (win === true) {
+				if ( winner === 1) { self.playerTurn = false; }
+					else { (self.playerTurn = true); }
+				// Clears the game board
+				GameBoard = ["","","","","","","","",""];
+				// Resets the rest of the game play variables
+				self.turnCounter = 0;
+				self.win = false;
+				self.turnCounter = 0;
+				self.playerMoves = ["", "", "", "", "", "", "", "", ""];
+			}
+		}
 
 	} // closes the Game Manager Function 
